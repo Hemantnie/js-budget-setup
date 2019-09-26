668f382f95d4755edcb7517d3aa2013a70b82053
Hemant Kumar <Hemant.Kumar@accolite.com>
2019-09-27 01:19:15 +0530

    Adding logic for simple expenditure caluculation

--- js/app.js
+++ js/app.js
@@ -15,4 +15,42 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+
+  submitBudgetForm(){
+    const value = this.budgetInput.value;
+    if(value === '' || value<0){
+      this.budgetFeedback.classList.add('showItem');
+      this.budgetFeedback.innerHTML = `<p>Value can not be empty or negative</p>`;
+      const self = this;
+      setTimeout(function(){
+        self.budgetFeedback.remove('showItem');
+      }, 4000);
+    }
+  }
 }
+
+function eventListners(){
+  const budgetForm = document.getElementById("budget-form");
+  const expenseForm = document.getElementById("expense-form");
+  const expenseList = document.getElementById("expense-list");
+
+  const ui = new UI();
+
+  budgetForm.addEventListener("submit",function (event){
+    event.preventDefault();
+    ui.submitBudgetForm();
+  });
+
+  expenseForm.addEventListener("submit",function (event){
+    event.preventDefault();
+    console.log("Submit from expenseForm: ",event);
+  });
+
+  expenseList.addEventListener("click",function (event){
+    event.preventDefault();
+    console.log("Submit from expenseList: ",event);
+  });
+
+}
+eventListners();
+
