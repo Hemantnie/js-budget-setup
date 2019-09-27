ec4debb4052e261465e92c24c9b06ede3e77b61b
Hemant Kumar <Hemant.Kumar@accolite.com>
2019-09-27 11:02:41 +0530

    adding js code for adding expenditure

--- js/app.js
+++ js/app.js
@@ -26,6 +26,79 @@ class UI {
         self.budgetFeedback.remove('showItem');
       }, 4000);
     }
+    else{
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = '';
+      this.showBalance();
+    }
+  }
+36
+  showBalance(){
+    const expense = this.totalExpense();
+    const total = parseInt(this.budgetAmount.textContent) - expense;
+    this.balanceAmount.textContent = total;
+    if(total < 0){
+      this.balance.classList.remove('showGreen','showBlack');
+      this.balance.classList.add('showRed');
+    }
+    else if(total > 0){
+      this.balance.classList.remove('showRed','showBlack');
+      this.balance.classList.add('showGreen');
+    }
+    else if(total === 0){
+      this.balance.classList.remove('showRed','showGreen');
+      this.balance.classList.add('showBlack');
+    }
+  }
+
+  submitExpenseForm(){
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value ;
+    if(expenseValue === '' || amountValue === '' || amountValue < 0){
+      this.expenseFeedback.classList.add('showItem');
+      this.expenseFeedback.innerHTML=`<p>Value can not be empty or negative`;
+      const self = this;
+      setTimeout(function(){
+        self.expenseFeedback.classList.remove("showItem");
+      },4000);
+    }
+    else{
+      let amount = parseInt(amountValue);
+      this.expenseInput.value = "";
+      this.amountInput.value = "" ;
+
+      let expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount:amount
+      }
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+    }
+  }
+
+  addExpense(expense){
+  const div = document.createElement('div');
+  div.classList.add('expense');
+  div.innerHTML = `
+  <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+         <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+         <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>
+  `;
+
   }
 }
 
@@ -43,7 +116,7 @@ function eventListners(){
 
   expenseForm.addEventListener("submit",function (event){
     event.preventDefault();
-    console.log("Submit from expenseForm: ",event);
+    ui.submitExpenseForm();
   });
 
   expenseList.addEventListener("click",function (event){
@@ -52,5 +125,10 @@ function eventListners(){
   });
 
 }
+
+document.addEventListener('DOMcontentLoaded',function(){
+  eventListners();
+})
+
 eventListners();
 
